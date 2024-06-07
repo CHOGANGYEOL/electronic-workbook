e39fb9d43d19c1d54bc147b48d1e49f4e255398d
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-07 15:57:25 +0900

    :memo: 필수계산문제 20선

---
+++ src/assets/data/UsingComputer/UC_2024_02/images/index.ts
@@ -0,0 +1,27 @@
+import Img_1 from './1.jpg';
+import Img_10 from './10.jpg';
+import Img_11 from './11.jpg';
+import Img_15 from './15.jpg';
+import Img_16 from './16.jpg';
+import Img_17 from './17.jpg';
+import Img_20 from './20.jpg';
+import Img_3 from './3.jpg';
+import Img_4 from './4.jpg';
+import Img_5 from './5.jpg';
+import Img_6_1 from './6_1.jpg';
+import Img_6_2 from './6_2.jpg';
+
+export default {
+	Img_1,
+	Img_3,
+	Img_4,
+	Img_5,
+	Img_6_1,
+	Img_6_2,
+	Img_10,
+	Img_11,
+	Img_15,
+	Img_16,
+	Img_17,
+	Img_20,
+};
--- src/assets/data/UsingComputer/index.tsx
+++ src/assets/data/UsingComputer/index.tsx
@@ -2,10 +2,11 @@ import { MOCK_QUESTION } from './MOCK_DATA';
 import { UC_2020_01 } from './UC_2020_01';
 import { UC_2020_02 } from './UC_2020_02';
 import { UC_2024_01 } from './UC_2024_01';
+import { UC_2024_02 } from './UC_2024_02';
 import { Data } from '../types';
 
 export const USING_COMPUTER: Data = {
 	title: '컴퓨터 활용 1급 필기',
 	key: 'USING_COMPUTER',
-	questions: [MOCK_QUESTION, UC_2024_01, UC_2020_02, UC_2020_01],
+	questions: [MOCK_QUESTION, UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
 };
