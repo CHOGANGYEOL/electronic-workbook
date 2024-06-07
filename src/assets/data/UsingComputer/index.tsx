253f54d6cb0465b7782d5b13e0229101edb521b7
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-07 16:51:01 +0900

    :wastebasket: Mock data delete

--- src/assets/data/UsingComputer/index.tsx
+++ src/assets/data/UsingComputer/index.tsx
@@ -1,4 +1,3 @@
-import { MOCK_QUESTION } from './MOCK_DATA';
 import { UC_2020_01 } from './UC_2020_01';
 import { UC_2020_02 } from './UC_2020_02';
 import { UC_2024_01 } from './UC_2024_01';
@@ -8,5 +7,5 @@ import { Data } from '../types';
 export const USING_COMPUTER: Data = {
 	title: '컴퓨터 활용 1급 필기',
 	key: 'USING_COMPUTER',
-	questions: [MOCK_QUESTION, UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
+	questions: [UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
 };
