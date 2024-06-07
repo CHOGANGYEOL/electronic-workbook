2ccddeb440f6247172fed625a1b6739fb5069375
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-07 17:50:03 +0900

    :memo: UC2024_03

---
+++ src/assets/data/UsingComputer/UC_2024_03/index.tsx
@@ -0,0 +1,7 @@
+import { Question } from '../../types';
+
+export const UC_2024_03: Question = {
+	questionId: 202403,
+	title: '2024대비 필수코드문제25선',
+	contents: [],
+};
--- src/assets/data/UsingComputer/index.tsx
+++ src/assets/data/UsingComputer/index.tsx
@@ -2,10 +2,11 @@ import { UC_2020_01 } from './UC_2020_01';
 import { UC_2020_02 } from './UC_2020_02';
 import { UC_2024_01 } from './UC_2024_01';
 import { UC_2024_02 } from './UC_2024_02';
+import { UC_2024_03 } from './UC_2024_03';
 import { Data } from '../types';
 
 export const USING_COMPUTER: Data = {
 	title: '컴퓨터 활용 1급 필기',
 	key: 'USING_COMPUTER',
-	questions: [UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
+	questions: [UC_2024_03, UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
 };
