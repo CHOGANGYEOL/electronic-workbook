2e2b9b8e509e9d22436fe9fd59daaacab5d66cb3
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-10 11:47:39 +0900

    :memo: 컴활1급 2024 키워드 50선

---
+++ src/assets/data/UsingComputer/UC_2024_05/images/index.ts
@@ -0,0 +1,13 @@
+import Img_20 from './20.jpg';
+import Img_22 from './22.jpg';
+import Img_24 from './24.jpg';
+import Img_25 from './25.jpg';
+import Img_34 from './34.jpg';
+
+export default {
+	Img_20,
+	Img_22,
+	Img_24,
+	Img_25,
+	Img_34,
+};
