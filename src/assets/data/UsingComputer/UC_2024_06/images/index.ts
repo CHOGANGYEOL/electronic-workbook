74f6b768b8d62c232e8fc4f84be39ecc0a78d88e
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-10 13:57:04 +0900

    :memo: 잘못된문장 200문제 중 100문제

---
+++ src/assets/data/UsingComputer/UC_2024_06/images/index.ts
@@ -0,0 +1,79 @@
+import Img_100 from './100.jpg';
+import Img_102 from './102.jpg';
+import Img_108 from './108.jpg';
+import Img_117_1 from './117_1.jpg';
+import Img_117_2 from './117_2.jpg';
+import Img_119 from './119.jpg';
+import Img_121_1 from './121_1.jpg';
+import Img_121_2 from './121_2.jpg';
+import Img_133 from './133.jpg';
+import Img_138 from './138.jpg';
+import Img_13_1 from './13_1.jpg';
+import Img_13_2 from './13_2.jpg';
+import Img_178_1 from './178_1.jpg';
+import Img_178_2 from './178_2.jpg';
+import Img_178_3 from './178_3.jpg';
+import Img_178_4 from './178_4.jpg';
+import Img_178_5 from './178_5.jpg';
+import Img_180_1 from './180_1.jpg';
+import Img_180_2 from './180_2.jpg';
+import Img_184_1 from './184_1.jpg';
+import Img_184_2 from './184_2.jpg';
+import Img_185_1 from './185_1.jpg';
+import Img_185_2 from './185_2.jpg';
+import Img_199 from './199.jpg';
+import Img_68 from './68.jpg';
+import Img_72 from './72.jpg';
+import Img_76 from './76.jpg';
+import Img_78 from './78.jpg';
+import Img_86_1 from './86_1.jpg';
+import Img_86_2 from './86_2.jpg';
+import Img_88 from './88.jpg';
+import Img_89 from './89.jpg';
+import Img_91 from './91.jpg';
+import Img_93_1 from './93_1.jpg';
+import Img_93_2 from './93_2.jpg';
+import Img_94 from './94.jpg';
+import Img_99_1 from './99_1.jpg';
+import Img_99_2 from './99_2.jpg';
+
+export default {
+	Img_13_1,
+	Img_13_2,
+	Img_68,
+	Img_72,
+	Img_76,
+	Img_78,
+	Img_86_1,
+	Img_86_2,
+	Img_88,
+	Img_89,
+	Img_91,
+	Img_93_1,
+	Img_93_2,
+	Img_94,
+	Img_99_1,
+	Img_99_2,
+	Img_100,
+	Img_102,
+	Img_108,
+	Img_117_1,
+	Img_117_2,
+	Img_119,
+	Img_121_1,
+	Img_121_2,
+	Img_133,
+	Img_138,
+	Img_178_1,
+	Img_178_2,
+	Img_178_3,
+	Img_178_4,
+	Img_178_5,
+	Img_180_1,
+	Img_180_2,
+	Img_184_1,
+	Img_184_2,
+	Img_185_1,
+	Img_185_2,
+	Img_199,
+};
--- src/assets/data/UsingComputer/index.tsx
+++ src/assets/data/UsingComputer/index.tsx
@@ -5,10 +5,11 @@ import { UC_2024_02 } from './UC_2024_02';
 import { UC_2024_03 } from './UC_2024_03';
 import { UC_2024_04 } from './UC_2024_04';
 import { UC_2024_05 } from './UC_2024_05';
+import { UC_2024_06 } from './UC_2024_06';
 import { Data } from '../types';
 
 export const USING_COMPUTER: Data = {
 	title: '컴퓨터 활용 1급 필기',
 	key: 'USING_COMPUTER',
-	questions: [UC_2024_05, UC_2024_04, UC_2024_03, UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
+	questions: [UC_2024_06, UC_2024_05, UC_2024_04, UC_2024_03, UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
 };
