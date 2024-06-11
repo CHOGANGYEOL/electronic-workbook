53d0d59b0c2db5b8d14ac9c255b3b0396da1815c
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-11 14:36:18 +0900

    :memo: 2023 상시 1회차

---
+++ src/assets/data/UsingComputer/UC_2023_01/images/index.ts
@@ -0,0 +1,37 @@
+import Img_26 from './26.jpg';
+import Img_27 from './27.jpg';
+import Img_28 from './28.jpg';
+import Img_29 from './29.jpg';
+import Img_31 from './31.jpg';
+import Img_35 from './35.jpg';
+import Img_36 from './36.jpg';
+import Img_38 from './38.jpg';
+import Img_40_1 from './40_1.jpg';
+import Img_40_2 from './40_2.jpg';
+import Img_40_3 from './40_3.jpg';
+import Img_40_4 from './40_4.jpg';
+import Img_40_5 from './40_5.jpg';
+import Img_57 from './57.jpg';
+import Img_58 from './58.jpg';
+import Img_60_1 from './60_1.jpg';
+import Img_60_2 from './60_2.jpg';
+
+export default {
+	Img_26,
+	Img_27,
+	Img_28,
+	Img_29,
+	Img_31,
+	Img_35,
+	Img_36,
+	Img_38,
+	Img_40_1,
+	Img_40_2,
+	Img_40_3,
+	Img_40_4,
+	Img_40_5,
+	Img_57,
+	Img_58,
+	Img_60_1,
+	Img_60_2,
+};
--- src/assets/data/UsingComputer/index.tsx
+++ src/assets/data/UsingComputer/index.tsx
@@ -1,5 +1,6 @@
 import { UC_2020_01 } from './UC_2020_01';
 import { UC_2020_02 } from './UC_2020_02';
+import { UC_2023_01 } from './UC_2023_01';
 import { UC_2024_01 } from './UC_2024_01';
 import { UC_2024_02 } from './UC_2024_02';
 import { UC_2024_03 } from './UC_2024_03';
@@ -11,5 +12,15 @@ import { Data } from '../types';
 export const USING_COMPUTER: Data = {
 	title: '컴퓨터 활용 1급 필기',
 	key: 'USING_COMPUTER',
-	questions: [UC_2024_06, UC_2024_05, UC_2024_04, UC_2024_03, UC_2024_02, UC_2024_01, UC_2020_02, UC_2020_01],
+	questions: [
+		UC_2024_06,
+		UC_2024_05,
+		UC_2024_04,
+		UC_2024_03,
+		UC_2024_02,
+		UC_2024_01,
+		UC_2023_01,
+		UC_2020_02,
+		UC_2020_01,
+	],
 };
