49142fcc21fcabd628d9f4a7e59be609b6eb9bfd
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-17 13:26:56 +0900

    :memo: 2023 상시3회차

---
+++ src/assets/data/UsingComputer/UC_2023_03/images/index.ts
@@ -0,0 +1,27 @@
+import Img_21 from './21.jpg';
+import Img_23 from './23.jpg';
+import Img_27 from './27.jpg';
+import Img_28 from './28.jpg';
+import Img_29 from './29.jpg';
+import Img_30 from './30.jpg';
+import Img_33 from './33.jpg';
+import Img_34 from './34.jpg';
+import Img_37 from './37.jpg';
+import Img_58 from './58.jpg';
+import Img_59 from './59.jpg';
+import Img_60 from './60.jpg';
+
+export default {
+	Img_21,
+	Img_23,
+	Img_27,
+	Img_28,
+	Img_29,
+	Img_30,
+	Img_33,
+	Img_34,
+	Img_37,
+	Img_58,
+	Img_59,
+	Img_60,
+};
