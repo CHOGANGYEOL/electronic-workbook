02a7d34ab055a02536fe5f48426bdc7f8742d198
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-20 16:33:58 +0900

    :memo: 2022 상시 2회차

--- src/assets/data/UsingComputer/UC_2022_02/images/index.ts
+++ src/assets/data/UsingComputer/UC_2022_02/images/index.ts
@@ -1,5 +1,21 @@
-// import Img_60_2 from './60_2.jpg';
+import Img_22 from './22.jpg';
+import Img_26 from './26.jpg';
+import Img_29 from './29.jpg';
+import Img_30 from './30.jpg';
+import Img_31 from './31.jpg';
+import Img_32 from './32.jpg';
+import Img_37 from './37.jpg';
+import Img_41 from './41.jpg';
+import Img_44 from './44.jpg';
 
 export default {
-	// Img_26,
+	Img_22,
+	Img_26,
+	Img_29,
+	Img_30,
+	Img_31,
+	Img_32,
+	Img_37,
+	Img_41,
+	Img_44,
 };
