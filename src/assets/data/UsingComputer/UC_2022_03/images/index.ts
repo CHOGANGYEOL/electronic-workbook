55a73f66333cf0af6724e2168d74c043a38a4403
CHOGANGYEOL <rkdduf06@icloud.com>
2024-06-28 11:29:12 +0900

    :memo: 차컴퓨터 활용 2022 3회차

--- src/assets/data/UsingComputer/UC_2022_03/images/index.ts
+++ src/assets/data/UsingComputer/UC_2022_03/images/index.ts
@@ -1,5 +1,41 @@
-// import Img_60_2 from './60_2.jpg';
+import Img_24 from './24.jpg';
+import Img_25 from './25.jpg';
+import Img_26 from './26.jpg';
+import Img_28 from './28.jpg';
+import Img_32_01 from './32_01.jpg';
+import Img_32_02 from './32_02.jpg';
+import Img_33 from './33.jpg';
+import Img_34 from './34.jpg';
+import Img_35 from './35.jpg';
+import Img_38 from './38.jpg';
+import Img_40 from './40.jpg';
+import Img_41_01 from './41_01.jpg';
+import Img_41_02 from './41_02.jpg';
+import Img_45_01 from './45_01.jpg';
+import Img_45_02 from './45_02.jpg';
+import Img_47 from './47.jpg';
+import Img_50 from './50.jpg';
+import Img_55 from './55.jpg';
+import Img_60 from './60.jpg';
 
 export default {
-	// Img_26,
+	Img_24,
+	Img_25,
+	Img_26,
+	Img_28,
+	Img_32_01,
+	Img_32_02,
+	Img_33,
+	Img_34,
+	Img_35,
+	Img_38,
+	Img_40,
+	Img_41_01,
+	Img_41_02,
+	Img_45_01,
+	Img_45_02,
+	Img_47,
+	Img_50,
+	Img_55,
+	Img_60,
 };
--- src/assets/data/UsingComputer/UC_2022_04/images/index.ts
+++ src/assets/data/UsingComputer/UC_2022_04/images/index.ts
@@ -1,5 +1,25 @@
-// import Img_60_2 from './60_2.jpg';
+import Img_24 from './24.jpg';
+import Img_25 from './25.jpg';
+import Img_26 from './26.jpg';
+import Img_27 from './27.jpg';
+import Img_28 from './28.jpg';
+import Img_32 from './32.jpg';
+import Img_38 from './38.jpg';
+import Img_39 from './39.jpg';
+import Img_41 from './41.jpg';
+import Img_54 from './54.jpg';
+import Img_57 from './57.jpg';
 
 export default {
-	// Img_26,
+	Img_24,
+	Img_25,
+	Img_26,
+	Img_27,
+	Img_28,
+	Img_32,
+	Img_38,
+	Img_39,
+	Img_41,
+	Img_54,
+	Img_57,
 };
