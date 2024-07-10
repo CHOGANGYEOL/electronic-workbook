8d8c72db71a49b45662d97b5ec2a9a4b2e4c80dc
CHOGANGYEOL <rkdduf06@icloud.com>
2024-07-10 10:08:37 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ self.addEventListener('install', async (event) => {
 			const cache = await caches.open(CACHE_NAME);
 			// Setting {cache: 'reload'} in the new request will ensure that the response
 			// isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-			await cache.add(new Request(path() + OFFLINE_URL, { cache: 'reload' }));
+			await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
 		})(),
 	);
 });
