b208909885e2ca9febebb1aad8c3a148fc53dc3a
cooper <cooper@gurufin.com>
2026-03-19 14:41:59 +0900

    feat: sangle plug

---
+++ src/assets/data/Tesat/TS_SANGGLE_PLUS_03/index.tsx
@@ -0,0 +1,157 @@
+import { Question } from '../../types';
+
+export const TS_SANGGLE_PLUS_03: Question = {
+	questionId: 'TS_SANGGLE_PLUS_03',
+	title: '생글 플러스 3월',
+	contents: [
+		{
+			contentId: 1,
+			question: '기업의 공식적인 실적 자료와 별개로 시장 참가자 사이에서 형성되는 실적 전망치를 뜻하는 말은?',
+			answer: ['어닝 서프라이즈', '오버 슈팅', '스트리트 컨센서스', '게임 체인저'],
+			correct: 3,
+		},
+		{
+			contentId: 2,
+			question: '다음 중 고소득층이거나 부자일수록 세율이 높아지는 세금이 아닌 것은?',
+			answer: ['법인세', '소득세', '상속세', '부가가치세'],
+			correct: 4,
+		},
+		{
+			contentId: 3,
+			question: '여성이 고위직으로 승진하기 어렵게 하는 ‘눈에 보이지 않는 차별’을 가리키는 말은?',
+			answer: ['포모', '갈라파고스', '양적완화', '유리천장'],
+			correct: 4,
+		},
+		{
+			contentId: 4,
+			question: '기업이 단기자금 조달을 위해 발행하는 것으로, 회사채에 비해 발행 절차가 간단하고 만기가 짧은 것은?',
+			answer: ['기업어음(CP)', '머니마켓펀드(MMF)', '주가연계증권(ELS)', '프라이빗뱅킹(PB)'],
+			correct: 1,
+		},
+		{
+			contentId: 5,
+			question:
+				'중국이 일본을 상대로 ‘이것’의 수출 규제를 16년 만에 발동했다. 컴퓨터, 전기차, 로봇, 반도체 등 첨단기술의 핵심 소재인 이것은?',
+			answer: ['웨이퍼', '희토류', '셰일오일', '브렌트유'],
+			correct: 2,
+		},
+		{
+			contentId: 6,
+			question: '주식 한 주의 액면가를 쪼개 주식 수를 늘리는 것으로, 주식의 유통 물량이 많아지게 되는 이 조치는?',
+			answer: ['배당', '유상증자', '액면분할', '상장폐지'],
+			correct: 3,
+		},
+		{
+			contentId: 7,
+			question: '불황기에 ‘작은 사치’를 누릴 수 있는 저렴한 소비재가 잘 팔리는 현상을 가리키는 말은?',
+			answer: ['립스틱 효과', '밴드왜건 효과', '자산 효과', '메기 효과'],
+			correct: 1,
+		},
+		{
+			contentId: 8,
+			question: '지난해 4분기에 한국 기업 최초로 ‘분기 영업이익 20조원 돌파’라는 신기록을 쓴 회사는?',
+			answer: ['삼성전자', 'SK하이닉스', '네이버', '카카오'],
+			correct: 1,
+		},
+		{
+			contentId: 9,
+			question: '65세 이상 고령자 가운데 소득 하위 70%에 매월 34만9700원을 지급하고 있는 복지제도는?',
+			answer: ['개인연금', '기초연금', '주택연금', '사적연금'],
+			correct: 2,
+		},
+		{
+			contentId: 10,
+			question: 'KODEX, TIGER, ACE, RISE 등은 어떤 금융상품에 붙는 브랜드일까?',
+			answer: ['ETF', 'CMA', 'IRP', 'IMA'],
+			correct: 1,
+		},
+		{
+			contentId: 11,
+			question:
+				'웨어러블 기기, 센서 등을 활용해 환자의 생체 데이터를 의료진에게 전송하는 ‘원격 환자 모니터링’을 뜻하는 용어는?',
+			answer: ['RPM', 'ODM', 'CAPEX', 'ETN'],
+			correct: 1,
+		},
+		{
+			contentId: 12,
+			question: '다음 중 프랑스 증시를 대표하는 주가지수를 고르면?',
+			answer: ['FTSE100', 'S&P500', 'CAC40', 'CSI300'],
+			correct: 3,
+		},
+		{
+			contentId: 13,
+			question:
+				'버크셔 해서웨이가 작년 4분기 ‘이 신문사’ 주식을 매입해 화제가 됐다. 미국의 대표 일간지 중 하나로, 뉴욕 증시 상장사이기도 한 이곳은?',
+			answer: ['월스트리트저널', '뉴욕타임스', '폴리티코', '워싱턴포스트'],
+			correct: 2,
+		},
+		{
+			contentId: 14,
+			question:
+				'기업이 정상적으로 영업활동을 하고 있음에도 일시적으로 현금 흐름이 막혀 이를 감당하지 못할 경우 벌어지는 상황은?',
+			answer: ['상장폐지', '기업공개', '분식회계', '흑자도산'],
+			correct: 4,
+		},
+		{
+			contentId: 15,
+			question: '개개인의 신용도를 평가해 부여하는 개인 신용점수는 몇 점이 만점일까?',
+			answer: ['100점', '200점', '1000점', '2000점'],
+			correct: 3,
+		},
+		{
+			contentId: 16,
+			question: '여러 자회사의 주식을 보유해 그룹 계열사를 지배하는 것이 주된 목적인 회사를 무엇이라 할까?',
+			answer: ['주식회사', '유한회사', '지주회사', '합작회사'],
+			correct: 3,
+		},
+		{
+			contentId: 17,
+			question:
+				'도널드 트럼프 미국 대통령의 대표 정책이던 ‘이것’이 연방대법원에서 위법 판결을 받아 무효가 됐다. 우리나라에는 15%가 적용됐던 이것은?',
+			answer: ['상호관세', '상계관세', '탄력관세', '보복관세'],
+			correct: 1,
+		},
+		{
+			contentId: 18,
+			question: '은행과 같은 전통 금융회사가 아니라 사모펀드나 자산운용사가 기업에 조달하는 자금을 뜻하는 용어는?',
+			answer: ['자산유동화증권', '토큰증권', '사모신용', '가계신용'],
+			correct: 3,
+		},
+		{
+			contentId: 19,
+			question: '제주항공, 진에어, 이스타항공, 티웨이항공의 공통된 사업인 ‘저비용항공사’를 가리키는 용어는?',
+			answer: ['MLCC', 'HBM', 'LCC', 'DSR'],
+			correct: 3,
+		},
+		{
+			contentId: 20,
+			question: '지난해 전 세계에서 연간 매출이 가장 많았던 기업은 어디일까?',
+			answer: ['엔비디아', '아람코', '월마트', '아마존'],
+			correct: 4,
+		},
+		{
+			contentId: 21,
+			question: '다음 중 주가가 하락할 때 수익이 나는 구조의 상장지수펀드(ETF)를 가리키는 용어는?',
+			answer: ['레버리지 ETF', '인버스 ETF', '커버드콜 ETF', '동일가중 ETF'],
+			correct: 2,
+		},
+		{
+			contentId: 22,
+			question: '다음 중 테슬라의 ‘완전자율주행’ 기술을 뜻하는 용어는?',
+			answer: ['FSD', 'FDI', 'FBI', 'FDA'],
+			correct: 1,
+		},
+		{
+			contentId: 23,
+			question: '텍스트, 이미지, 영상, 음성 등 다양한 형식의 데이터를 통합해 이해하고 처리하는 인공지능(AI) 기술은?',
+			answer: ['하이퍼스케일러', 'ESS', '중간배당', '멀티모달'],
+			correct: 4,
+		},
+		{
+			contentId: 24,
+			question: '기업이 벌어들이는 ‘이익’에 비해 주가가 어느 정도 수준인지를 평가하는 밸류에이션 지표는?',
+			answer: ['PER', 'PBR', 'PSR', 'ROE'],
+			correct: 1,
+		},
+	],
+};
--- src/assets/data/Tesat/index.ts
+++ src/assets/data/Tesat/index.ts
@@ -1,9 +1,12 @@
 import { TS_2026_03 } from './TS_2026_03';
 import { TS_SANGGLE } from './TS_SANGGLE';
+import { TS_SANGGLE_PLUS_01 } from './TS_SANGGLE_PLUS_01';
+import { TS_SANGGLE_PLUS_02 } from './TS_SANGGLE_PLUS_02';
+import { TS_SANGGLE_PLUS_03 } from './TS_SANGGLE_PLUS_03';
 import { Data } from '../types';
 
 export const TESAT: Data = {
 	title: 'TESAT',
 	key: 'TESAT',
-	questions: [TS_2026_03, TS_SANGGLE],
+	questions: [TS_2026_03, TS_SANGGLE, TS_SANGGLE_PLUS_03, TS_SANGGLE_PLUS_02, TS_SANGGLE_PLUS_01],
 };
